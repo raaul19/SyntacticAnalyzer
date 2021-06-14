6151eb404409b85c7db31b1bcc52b225643a15f4
Raul Garcia <raul.jaime.garcia.espejo@gmail.com>
2021-06-14 17:37:17 -0500

    Syntactic files finished

--- SyntacticAnalyzer/syntactic.cpp
+++ SyntacticAnalyzer/syntactic.cpp
@@ -22,10 +22,76 @@ Syntactic::~Syntactic(){
 }
 
 void Syntactic::fillArrays(){
+    for(int i(0); i < TABLE_ROWS; i++){
+        for (int j(0); j < TABLE_COLS; j++){
+            tableFile >> table[i][j];
+        }
+    }
 
+    for(int i(0); i < RULES_ROWS; i++){
+        for (int j(0); j < RULES_COLS; j++){
+            rulesFile >> rules[i][j];
+        }
+    }
 }
 
 bool Syntactic::analyze(string code){
+    Automatum automatum;
+    list<Analyzer> aux;
+    vector<int> numbers;
+    stack<int> testStack;
+    int num, numTest, rule;
+    bool isAccepted = false;
+
+    aux = automatum.automata(code);
+    fillArrays();
+    if(!aux.empty()){
+       for(std::list<Analyzer>::iterator ite = aux.begin(); ite != aux.end(); ite++){
+            stringstream ss(ite->getNum());
+            ss >> num;
+            numbers.push_back(num);
+        }
+    }
+
+    testStack.push(0);
+    for(int i (0); i < numbers.size();i++){
+        numTest = table[testStack.top()][numbers[i]];
+
+        if(numTest > 0){//positive
+            testStack.push(numbers[i]);
+            testStack.push(numTest);
+        }
+        else if(numTest < 0){// negative == rule
+            if(numTest == -1){
+                isAccepted = true;
+                break;
+            }
+
+            numTest= (numTest*-1);
+            numTest = numTest -1;
+
+            if((rule = rules[numTest][NUM_POPS]) == 0){
+                //no pops
+            }
+            else{// pop
+                rule*=2;
+                for(int k(0); k < rule; k++){
+                    testStack.pop();
+                }
+            }
+            numTest = table[testStack.top()][rule = rules[numTest][RULE]];
+            testStack.push(rule);
+            testStack.push(numTest);
+            i--;
+        }
+        else{// not defined
+            break;
+        }
+    }
+    while(!testStack.empty()){
+        testStack.pop();
+    }
+    return isAccepted;
 
 }
 
--- SyntacticAnalyzer/syntactic.h
+++ SyntacticAnalyzer/syntactic.h
@@ -3,6 +3,11 @@
 
 #include <fstream>
 #include "automatum.h"
+#include "analyzer.h"
+#include <list>
+#include <vector>
+#include <sstream>
+#include <stack>
 
 #define TABLE_ROWS 84
 #define TABLE_COLS 39
@@ -10,6 +15,9 @@
 #define RULES_COLS 2
 #define TABLE_FILE "GR2slrTable.txt"
 #define RULES_FILE "GR2slrRulesId.txt"
+
+#define RULE 0
+#define NUM_POPS 1
 
 class Syntactic{
 private:
